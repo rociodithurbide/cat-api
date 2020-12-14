cbbacf57d37aebb1fa34a476dcbc97252df37d0f
Rocio <rocio.dithurbide@gmail.com>
2020-12-14 11:14:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./reset.css";
+import "./App.scss";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const getData = await fetch("https://api.thecatapi.com/v1/images/search");
+    const json = await getData.json();
+
+    //console.log(json);
+
+    const url = json.map((element, key) => {
+      const catImg = element.url;
+      console.log(catImg);
+      return catImg;
+    });
+
+    //setData(json[0].url);
+    setData(url);
+    setLoading(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading === true ? (
+        <p>Cargando Gatito...</p>
+      ) : (
+        <>
+          <div className="imgWrapper">
+            <img className="catImg" src={data} alt="Cat Images" />
+          </div>
+          <button onClick={fetchData} className="button">
+            Otro Gatito
+          </button>
+        </>
+      )}
     </div>
   );
 }
